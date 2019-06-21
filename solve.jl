76cb38facc259a781cbb051515dace8a863cc5c9
ky-s <kyohei_86@hotmail.com>
2019-06-21 23:24:01 +0900

    add 2patterns

--- solve.jl
+++ solve.jl
@@ -5,3 +5,15 @@ function word_compress(word1::String, word2::String)
     word1 * word2
 end
 
+function word_compress2(word1::String, word2::String)
+    i = findlast(size -> endswith(word1, word2[1:size]), 1:length(word2))
+    i == nothing && (i = 0)
+
+    word1 * word2[i+1:end]
+end
+
+function word_compress3(word1::String, word2::String)
+    findlast(size -> endswith(word1, word2[1:size]), 1:length(word2)) |>
+    i -> i == nothing ? 0 : i |>
+    i -> word1 * word2[i+1:end]
+end
--- test.jl
+++ test.jl
@@ -3,4 +3,6 @@ include("./solve.jl")
 data = ["paiza", "apple", "letter"]
 
 @assert "paizappletter" == reduce(word_compress, data)
+@assert "paizappletter" == reduce(word_compress2, data)
+@assert "paizappletter" == reduce(word_compress3, data)
 
